4b9c73a6174421221d3e6a599b675707c85935d6
Meher Askri <meheraskri@gmx.com>
2024-05-21 06:10:05 +0100

    bind_project

---
+++ myscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+myvar=$(ip -br ad sh | grep -v lo | awk '{print $3}' | cut -d / -f1)
+myrev=$(ip -br ad sh | grep -v lo | awk '{print $3}' | awk -F . '{print $4}' | cut -d / -f1)
+nsupdate << EOF
+server 192.168.100.204
+zone meher.tn
+update add $HOSTNAME. 86400 A $myvar
+send
+quit
+EOF
+
+nsupdate << MOR
+server 192.168.100.204
+zone 100.168.192.in-addr.arpa
+update add $myrev.100.168.192.in-addr.arpa 86400 PTR $HOSTNAME.
+send
+quit
+MOR
